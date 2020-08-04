3355dc888ea1b39329aa8823c0ca06b31cd9356c
CurtisM <curtismorice@gmail.com>
2020-08-04 08:14:03 -0500

    Added company website

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -26,11 +26,18 @@ const ProjectsPage = () => {
           <h2 className="link">Welspun</h2>
         </a>
         <a
-          href="https://webcenter.sisyphus-industries.com/users/sign_in"
+          href="https://sisyphus-industries.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="link">Sisyphus Industries</h2>
+        </a>
+        <a
+          href="https://webcenter.sisyphus-industries.com/users/sign_in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="link">Sisyphus Industries Web Center</h2>
         </a>
         <a
           href="https://play.google.com/store/apps/details?id=com.sisbot.sisyphus&hl=en_US"
